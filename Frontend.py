f5099f48aab0af00ed01aa02ea7404f297943dc7
Rithika <126352441+Rika290@users.noreply.github.com>
2023-12-19 13:51:52 +0530

    Update Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 # from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 import plotly.express as px
